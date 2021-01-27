2a89c9e46a090653a93012aab61fa1f3d552ce72
Njabulo Brighton Chitsiko <48308828+NjabuloBrighton@users.noreply.github.com>
2021-01-27 20:42:34 +0200

    Work in progress. Tutorial based.
    Generic blog webapp

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Blog
@@ -14,7 +17,36 @@ namespace Blog
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+
+            var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            ctx.Database.EnsureCreated();
+
+            var adminRole = new IdentityRole("Admin");
+
+            if (!ctx.Roles.Any())
+            {
+                roleMgr.CreateAsync(adminRole).GetAwaiter().GetResult() ;
+                //create a role
+            }
+
+            if (!ctx.Users.Any(u => u.UserName == "Admin"))
+            {
+                //create a admin
+                var adminUser = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+                userMgr.CreateAsync(adminUser, "password");
+        }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog
 {
     public class Startup
     {
+
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
+
+            services.AddTransient<IRepository, Repository>();
+
+            services.AddMvc();
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +47,16 @@ namespace Blog
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
+
+
+            app.UseAuthentication();
+
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
